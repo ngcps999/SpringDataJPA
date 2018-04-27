77f34f7379abe20c59c9bcdf2293c242467d1de4
xuda <ngcps999@163.com>
2018-04-27 15:45:30 +0800

    create

--- src/main/java/com/fyerp/admin/config/CorsConfig.java
+++ src/main/java/com/fyerp/admin/config/CorsConfig.java
@@ -10,6 +10,9 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
+/**
+ * 全局跨域请求
+ */
 @Configuration
 public class CorsConfig extends WebMvcConfigurerAdapter {
 
