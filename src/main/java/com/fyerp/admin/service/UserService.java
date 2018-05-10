77797b1563e476c195332495dfbd231b26e96bf4
xuda <ngcps999@163.com>
2018-05-10 14:58:12 +0800

    create

--- src/main/java/com/fyerp/admin/service/UserService.java
+++ src/main/java/com/fyerp/admin/service/UserService.java
@@ -14,8 +14,6 @@ import com.fyerp.admin.domain.User;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
-import java.util.List;
-
 public interface UserService {
 	
 	/**通过username查找用户信息;*/
