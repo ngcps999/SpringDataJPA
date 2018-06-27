beff961d69c736921e1b7998b797d768dd8a63a9
xiasc <983732656@qq>
2018-06-27 08:48:57 +0800

    @xiasc shiro

--- src/main/java/com/fyerp/admin/config/CorsConfig.java
+++ src/main/java/com/fyerp/admin/config/CorsConfig.java
@@ -18,7 +18,7 @@ public class CorsConfig extends WebMvcConfigurerAdapter {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("**")
+        registry.addMapping("/**")
                 .allowedOrigins("*")
                 .allowedHeaders("*")
                 .allowCredentials(true)
